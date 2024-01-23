7c2dd7e2b63755fc87ad9f742c687237464dcf11
LeonFen03 <LeonFen03@gmail.com>
2024-01-22 20:38:57 -0500

    Set up base Authentication router

--- controller/authenticationRouter.js
+++ controller/authenticationRouter.js
@@ -3,3 +3,19 @@ const authenticationRouter = require('express').Router();
 const bcrypt = require('bcrypt');
 const jwt = require('json-web-token');
 const userAuth = require('../models/UserAuthenticationModel');
+
+authenticationRouter.post('/', async (req, res) => {
+    
+    let user = await userAuth.findOne({
+        email: req.body.email 
+    })
+
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({
+        message: `Could not find a user with the provided username and password` 
+      })
+    } else {
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user._id })           
+        res.json({ user: user, token: result.value })           
+    }
+})
