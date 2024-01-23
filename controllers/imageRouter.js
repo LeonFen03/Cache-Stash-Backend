2ede602c9711c00edf007161dfd0c5d3983f761a
LeonFen03 <LeonFen03@gmail.com>
2024-01-22 21:37:15 -0500

    Fixed Human Error in file import

--- controllers/imageRouter.js
+++ controllers/imageRouter.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const imageRouter = express.Router();
 const mongoose = require('mongoose');
-const imageDB = require('../models/images');
-const bodyParser = require('body-parser');
+const imageDB = require('../models/ImagesModel');
 const { S3Client, DeleteObjectCommand } = require("@aws-sdk/client-s3");
