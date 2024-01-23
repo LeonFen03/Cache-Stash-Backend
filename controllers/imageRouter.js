009c3af3b1dc323befe1d516e4758e19add904d0
LeonFen03 <LeonFen03@gmail.com>
2024-01-22 21:38:58 -0500

    Synced s3Client configuration on both javascript files

--- controllers/imageRouter.js
+++ controllers/imageRouter.js
@@ -3,3 +3,11 @@ const imageRouter = express.Router();
 const mongoose = require('mongoose');
 const imageDB = require('../models/ImagesModel');
 const { S3Client, DeleteObjectCommand } = require("@aws-sdk/client-s3");
+
+const s3Client = new S3Client({
+    region: process.env.AWS_REGION, 
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+});
