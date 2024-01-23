f675929fde232470615cb8ba1171242276aa73b5
LeonFen03 <LeonFen03@gmail.com>
2024-01-22 21:31:24 -0500

    Added API credentials

--- controllers/ImageUploadRouter.js
+++ controllers/ImageUploadRouter.js
@@ -6,3 +6,12 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const imageDB = require('../models/ImagesModel');
 
+const s3Client = new S3Client({
+    region: "us-east-1",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
+});
+
+const upload = multer({ dest: 'uploads/' });
